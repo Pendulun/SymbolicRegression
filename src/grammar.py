a0ccdb7a47b350412500a11cf500674f61791a42
Daniel Campos <danielcampos14@hotmail.com.br>
2023-05-04 09:16:24 -0300

    Grammar, Rule, Expansion

--- src/grammar.py
+++ src/grammar.py
@@ -1,22 +1,118 @@
+from __future__ import annotations
+
 class Grammar():
     def __init__(self):
         self.all_rules = {}
         self.terminal_rules = list()
         self.non_terminal_rules = list()
     
-    def rule(self, rule_str:str)->list:
+    def rule(self, rule_str:str)->Rule:
         return self.all_rules[rule_str]
     
-    def add_terminal_rule(self, rule_name:str, rule_expansions:list):
-        if rule_name not in self.terminal_rules:
-            self.terminal_rules.append(rule_name)
+    def add_terminal_rule(self, rule:Rule):
+        if rule.name not in self.terminal_rules:
+            self.terminal_rules.append(rule.name)
         
         #this overrides previous definition
-        self.all_rules[rule_name] = rule_expansions
+        self.all_rules[rule.name] = rule
     
-    def add_non_terminal_rule(self, rule_name:str, rule_expansions:list):
-        if rule_name not in self.non_terminal_rules:
-            self.non_terminal_rules.append(rule_name)
+    def add_non_terminal_rule(self, rule:Rule):
+        if rule.name not in self.non_terminal_rules:
+            self.non_terminal_rules.append(rule.name)
         
         #this overrides previous definition
-        self.all_rules[rule_name] = rule_expansions
+        self.all_rules[rule.name] = rule
+    
+    def __str__(self):
+        grammar_str = ""
+        for rule in self.non_terminal_rules:
+            grammar_str += str(self.all_rules[rule])+"\n"
+        
+        for rule in self.terminal_rules:
+            grammar_str += str(self.all_rules[rule])+"\n"
+        
+        grammar_str = grammar_str.strip('\n')
+        return grammar_str
+
+class Rule():
+    def __init__(self, name:str, expansions:list[Expansion]):
+        self._name = name
+        self._expansions = expansions
+    
+    def at(self,expansion_idx:int) -> Expansion:
+        return self.expansions[expansion_idx]
+    
+    @property
+    def expansions(self) -> list[Expansion]:
+        return self._expansions
+    
+    @expansions.setter
+    def expansions(self, new_expansions):
+        raise AttributeError("expansions is not subscriptable")
+    
+    @property
+    def name(self)->str:
+        return self._name
+
+    @name.setter
+    def name(self, new_name:str):
+        raise AttributeError("Name is not subscriptable")
+    
+    def __str__(self):
+        rule_str = self._name+": "
+        for expansion in self._expansions:
+            rule_str += str(expansion) +" | "
+        
+        rule_str = rule_str.strip()[:-2]
+        return rule_str
+
+class Expansion():
+    def __init__(self, expansion_terms:str|int|float|tuple):
+        self._expansion_terms = expansion_terms
+    
+    @property
+    def terms(self):
+        return self._expansion_terms
+    
+    @terms.setter
+    def terms(self, new_terms):
+        raise AttributeError("terms is not subscriptable")
+
+    def __eq__(self, __value: object) -> bool:
+        if not isinstance(__value, Expansion):
+            return False
+        
+        if __value.terms == self.terms:
+            return True
+        
+        return False
+
+    def __str__(self):
+        exp_str = ""
+        for term in self._expansion_terms:
+            exp_str += str(term) + " "
+        
+        exp_str = exp_str.strip()
+        return exp_str
+
+class NumericExp(Expansion):
+    @property
+    def terms(self):
+        return self._expansion_term
+    
+    def __init__(self, expansion_term: int| float):
+        self._expansion_term = expansion_term
+    
+    def __str__(self):
+        return str(self._expansion_term)
+
+class StrExp(Expansion):
+    @property
+    def terms(self):
+        return self._expansion_term
+    
+    def __init__(self, expansion_term: str):
+        self._expansion_term = expansion_term
+    
+    def __str__(self):
+        return self._expansion_term
--- src/tests/test_grammar.py
+++ src/tests/test_grammar.py
@@ -1,25 +1,48 @@
 from unittest import main, TestCase
-from grammar import Grammar
+from grammar import Grammar, Rule, Expansion, NumericExp, StrExp
 
 class TestGrammar(TestCase):
 
     def test_can_create_grammar(self):
-        non_terminal_rules = {
-            'expr': [('term','binop','term'), ('unop', 'term')],
-            'term': [('var'), ('const')]
-        }
-        terminal_rules = {
-            'var': [('X1'), ('X2'), ('X3')],
-            'const': [(1), (2), (3), (4)]
-        }
-        grammar = Grammar()
-        for rule_name, rule_exp in non_terminal_rules.items():
-            grammar.add_non_terminal_rule(rule_name, rule_exp)
-        
-        for rule_name, rule_exp in terminal_rules.items():
-            grammar.add_terminal_rule(rule_name, rule_exp)
+        non_terminal_rules = [
+            Rule('expr', [Expansion(('term','binop','term')), Expansion(('unop', 'term'))]),
+            Rule('term', [StrExp('var'), StrExp('const')])
+        ]
 
-        self.assertListEqual(grammar.rule('var'), [('X1'),('X2'),('X3')])
+        terminal_rules = [
+            Rule('var', [StrExp('X1'), StrExp('X2'), StrExp('X3')]),
+            Rule('const', [NumericExp(1), NumericExp(2), NumericExp(3), NumericExp(4)])
+        ]
+        grammar = Grammar()
+        for rule in non_terminal_rules:
+            grammar.add_non_terminal_rule(rule)
+        
+        for rule in terminal_rules:
+            grammar.add_terminal_rule(rule)
+
+        expected_terms = [StrExp('X1'), StrExp('X2'), StrExp('X3')]
+        self.assertEqual(grammar.rule('var').expansions, expected_terms)
+    
+    def test_can_print_grammar(self):
+        non_terminal_rules = [
+            Rule('expr', [Expansion(('term','binop','term')), Expansion(('unop', 'term'))]),
+            Rule('term', [StrExp('var'), StrExp('const')])
+        ]
+
+        terminal_rules = [
+            Rule('var', [StrExp('X1'), StrExp('X2'), StrExp('X3')]),
+            Rule('const', [NumericExp(1), NumericExp(2), NumericExp(3), NumericExp(4)])
+        ]
+        grammar = Grammar()
+        for rule in non_terminal_rules:
+            grammar.add_non_terminal_rule(rule)
+        
+        for rule in terminal_rules:
+            grammar.add_terminal_rule(rule)
+        
+        expected_grammar_str = "expr: term binop term | unop term\nterm: var | const\nvar: X1 | X2 | X3\nconst: 1 | 2 | 3 | 4"
+        self.assertEqual(str(grammar), expected_grammar_str)
+
 
 if __name__ == "__main__":
     main()
